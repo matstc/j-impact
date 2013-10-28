76a2e778aacec4e44a0093514f49870f9cdc3765
Matt Tanguay-Carel <matt@matthias>
2013-10-28 20:26:17 +0100

    seeded a few events

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,4 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 outlets = Outlet.create([{name: 'New York Times'},{name: 'Washington Post'}, {name: 'The Tyee'}])
 stories = Story.create([{headline: 'Dying fish'},{headline: 'Corruption strikes back'},{headline: 'Unwarranted promotion'}])
+events = Event.create([{event_type: 'Local pickup', story_id: stories[0].id},{event_type: 'blog post', story_id: stories[0].id}])
